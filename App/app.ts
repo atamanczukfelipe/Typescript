75b8950f2177be15fff25a24bae2b99cb9624cf8
Felipe <fatamanczuk@degdigital.com>
2022-04-16 09:07:51 -0300

    App main

---
+++ App/app.ts
@@ -0,0 +1,9 @@
+let nome: string;
+let sobrenome: string;
+let idade: number;
+
+nome = "Felipe";
+sobrenome = "Atamanczuk"
+idade = 30;
+
+console.log(`Nome: ${nome} ${sobrenome} e tem idade: ${idade}`);
