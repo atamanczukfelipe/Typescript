f201690d9a2e06a7418a74a27f3d96b08ed0cdfd
Felipe <fatamanczuk@degdigital.com>
2022-04-24 18:33:15 -0300

    Class Decorator

--- App2/Decorators.ts
+++ App2/Decorators.ts
@@ -1,3 +1,5 @@
+import { setSourceMapRange } from "./node_modules/typescript/lib/typescript";
+
 /*Decorator Syntax
 @name (params)
 */
@@ -7,7 +9,29 @@ function greeting( msg:string){
     }
 }
 
+function setOS(os: string){
+    return (target: any) => {
+        return class extends target{
+            version = os;
+        }
+    }
+}
+
+//Decorator Greeting
 @greeting('Olá seja bem vindo!')
 class Pessoa{
 
 }
+@setOS('WebOs 2.0')
+class Lg{
+
+}
+let tvLg = new Lg();
+console.log(tvLg);
+@setOS('Android TV 3.0')
+class Sony{
+
+}
+
+let tvSony = new Sony();
+console.log(tvSony);
